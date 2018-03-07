afb142051f49a7c2ca34a8cd8f2a8c3c1cf84465
RaduMilici <Radu.Milici.3pg@phishme.com>
2018-03-07 09:11:41 +0200

    results

--- src/kairos.js
+++ src/kairos.js
@@ -1,6 +1,6 @@
 import { post } from 'request';
 
-const url = 'http://api.kairos.com/detect';
+const url = 'https://api.kairos.com/detect';
 const headers = {
   'app_id': '5623987f',
   'app_key': '6c40ec661f881b50a65b5700e91ba3f1'
